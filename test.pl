fe850ffd0d675e69cef533cf6b53946385551489
Nelo Onyiah <nelo.onyiah@congenica.com>
2025-01-03 16:42:09 +0200

    use a local file not web url

--- test.pl
+++ test.pl
@@ -5,7 +5,7 @@ use XML::LibXML ();
 
 my $dom = XML::LibXML->load_xml( string => <<'EOF' );
 <?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE foo [<!ENTITY xxe SYSTEM "http://webhook.site/0b5480b4-229c-4531-98f8-f39506257df3">]>
+<!DOCTYPE foo [<!ENTITY xxe SYSTEM "file:///app/readme.md">]>
 <foo>&xxe;</foo>
 EOF
 
